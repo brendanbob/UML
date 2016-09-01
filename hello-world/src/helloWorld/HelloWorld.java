83bfa9e62a259636abc93c0281ef90ab2d75f95a
brendanbob <bleahy20@icloud.com>
2016-09-01 13:40:21 -0400

    Change-Id: I277933f5571e1d03d874652598e2a659d191e34c

--- hello-world/src/helloWorld/HelloWorld.java
+++ hello-world/src/helloWorld/HelloWorld.java
@@ -1,14 +1,14 @@
 package helloWorld;
-/*
- * /------------------------------------------------------------------------\
- * | The following programs are the sole property of Avid Technology, Inc.,	|
- * | and contain its proprietary and confidential information.          	|
- * |             Copyright 2016 by Avid Technology, Inc.    	    	    |
- * \------------------------------------------------------------------------/
- */
 
+
+/**
+ * 
+ */
 public class HelloWorld {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println("Why hello there world!");
 		System.out.println("How are you doing today?");
